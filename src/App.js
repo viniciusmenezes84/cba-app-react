cffedd278170e1c95440259637ae52b145d3b2a3
Vinicius <vinicius.m84@gmail.com>
2025-07-31 14:14:48 -0300

    site-cba_V6.9.1

--- src/App.js
+++ src/App.js
@@ -2265,7 +2265,7 @@ export default function App() {
     const [auth, setAuth] = useState({ status: 'unauthenticated', user: null, error: null });
     const [librariesLoaded, setLibrariesLoaded] = useState(false);
     
-    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby5--HfZ840iF66l36ILdbrvBQ6DW74kAbPmPgterp2oqCN9qCSD59JoFvbmT_pvIbFtA/exec";
+    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwNXGI4Cc5qGBye-IfWW_qqUcJ04NfArulExPXE4jgX0SZhWAmeWCjjKg2U9FFfHkHE/exec";
 
     const handleLogin = async (e) => {
         e.preventDefault();
