90c52b8437487adc2600261bae62fd616f128909
viniciusmenezes84 <vinicius.m84@gmail.com>
2026-09-15 20:44:03 -0300

    fix: use hardened PDF reports bridge

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import PresenceDashboardBridge from './PresenceDashboardBridge';
 import ReportsDashboardBridge from './ReportsDashboardBridge';
-import ReportsPdfBridge from './ReportsPdfBridge';
+import ReportsPdfBridgeV2 from './ReportsPdfBridgeV2';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +14,7 @@ root.render(
       <App />
       <PresenceDashboardBridge />
       <ReportsDashboardBridge />
-      <ReportsPdfBridge />
+      <ReportsPdfBridgeV2 />
     </>
   </React.StrictMode>
 );
