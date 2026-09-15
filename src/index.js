a74c1d90a36af2d95044c3d7f4c120be643467e4
viniciusmenezes84 <vinicius.m84@gmail.com>
2026-09-15 20:42:04 -0300

    feat: enable PDF reports v2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import PresenceDashboardBridge from './PresenceDashboardBridge';
 import ReportsDashboardBridge from './ReportsDashboardBridge';
+import ReportsPdfBridge from './ReportsPdfBridge';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,6 +14,7 @@ root.render(
       <App />
       <PresenceDashboardBridge />
       <ReportsDashboardBridge />
+      <ReportsPdfBridge />
     </>
   </React.StrictMode>
 );
