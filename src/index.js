eed9c99bc6dcb1c0238c17241cb45e5b11152f38
viniciusmenezes84 <vinicius.m84@gmail.com>
2026-09-16 10:52:08 -0300

    Ativa layout compacto do Mesário

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './reports-pdf-v2.css';
+import './mesario-compact.css';
 import App from './App';
 import PresenceDashboardBridge from './PresenceDashboardBridge';
 import ReportsDashboardBridge from './ReportsDashboardBridge';
